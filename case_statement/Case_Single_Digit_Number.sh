e75218d8a27f53300fcb63caf90bde1701019e76
Ashish <asaschoudhary616@gmail.com>
2020-02-22 09:20:38 +0530

    Added Case_Single_Digit_Number.sh

---
+++ case_statement/Case_Single_Digit_Number.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Enter The Number"
+read number
+
+case $number in
+	0)	
+   	echo "Zero";;
+	1)
+		echo "One";;
+	2)
+		echo "Two";;
+	3)
+		echo "three";;
+	4)
+		echo "Four";;
+	5)
+		echo "Five";;
+	6)
+		echo "Six";;
+	7)
+		echo "Seven";;
+	8)
+		echo "Eight";;
+	9)
+		echo "Nine";;
+	10)
+		echo "Ten";;
+esac
+
+
+
+
