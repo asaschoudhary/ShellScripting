b1eb58eec7e82a2d4cfc7a5af6946124d5b7fff8
Ashish <asaschoudhary616@gmail.com>
2020-02-22 09:08:58 +0530

    Added Arithmetic_Operation_Max_max.sh

---
+++ if_elif_else_statement/Arithmetic_Operation_Max_Min.sh
@@ -0,0 +1,22 @@
+echo "Enter the number"
+read a
+read b
+read c
+minValue=$a
+maxValue=$a
+if [ $minValue -lt $b ]
+then
+  minvalue=$b
+elif [$minValue -lt $c ]
+echo $minvalue 
+if [ $maxValue -gt $b ]
+then
+   maxValue=$b
+elif [ $maxValue -gt $c ]
+then
+   mixValue=$c
+echo $maxValue
+if
+if
+
+
