3c0e328fcb762bf9dce67411753dbcac03dd4ac5
Ashish <asaschoudhary616@gmail.com>
2020-02-24 12:59:30 +0530

    MODIFIED : Changed comment

--- case_statement/UnitConversion.sh
+++ case_statement/UnitConversion.sh
@@ -1,32 +1,36 @@
-#!/bin/bash
-echo "Enter Number between 1 to 3"
+#!/bin/bash -x
+echo "Enter number between 1 to 4"
 read number
-
 case $number in
 	1)
 		echo"convert feet into inch"
 		echo "enter feet"
 		read feet
 		inch=$((feet*12))
-		echo "feet: $inch";;
+		echo "feet: $inch"
+		;;
 	2)
 		echo"convert inch into feet"
 		echo "enter inch"
 		read inch
 		feet='echo"scale=3;$inch/12" | bc'
-		echo "feet :$feet";;
+		echo "feet :$feet"	
+		;;
 	3)
 		echo "convert feet into meter"
 		echo "enter feet"
 		read feet
 		meter='enter"scale=3;$feet/3.128"| bc'
-		echo "meter :$meter";;
+		echo "meter :$meter"	
+		;;
 	4)
 		echo "convert meter into feet"
 		echo "enter meter"
 		read meter
 		feet='echo"scale=3;$meter*3.128" | bc'
-		echo "feet:$feet";;
+		echo "feet:$feet"	
+		;;
 	*)
-		echo"invalid"
+		echo"invalid number"
+		;;
 esac
