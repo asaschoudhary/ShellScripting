c70737c6d9910849b4973b23acfba7bb02720db7
Ashish <asaschoudhary616@gmail.com>
2020-02-24 12:35:23 +0530

    MODIFIED : Changed variable

--- case_statement/Number_Week_Day.sh
+++ case_statement/Number_Week_Day.sh
@@ -1,32 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
+#Logic for printing week days 
 echo "enter The Number"
 read number
 case $number in
+	 #Enter 0 it will print sunday and so on so on upto 6 number 
 	0)
 	echo "Sunday"
 	;;
-
 	1)	
 	echo "Monday"
 	;;
-
 	2)
 	echo "Tuesday"
 	;;
-
 	3)
 	echo "Wednesday"
 	;;
-
- 	4)
-	echo "Thrusday"
+	4)
+	echo "Thursday"
 	;;
-
 	5)	
 	echo "Friday"
 	;;
-
 	6)
 	 echo "Saturday"
+	;;
+	*)
+	echo "invalid number"
 	;;
 esac
